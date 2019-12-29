d39bc63e0f0f41b0f7efc3ba523971493a92996e
Mohammed Remeez <remizhere@gmail.com>
2019-12-29 15:42:56 +0530

    new function to create Nonce to a URL.

--- src/index.php
+++ src/index.php
@@ -8,4 +8,10 @@ class Index
 	{
 		return 123345656;
 	}
+
+	public function createNonceUrl($url, $postWithId, $nonceName = "my_nonce")
+	{	
+		$field = "<input type='hidden' name='".$nonceName."' value='".$this->createNonceField()."'>"
+		return $field;
+	}
 }
