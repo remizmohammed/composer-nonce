1e490f1cec76dfe46bc4910a22bcfdfcc1b0637a
Mohammed Remeez <remizhere@gmail.com>
2019-12-30 10:47:26 +0530

    Testing wordpress function calls in composer.

--- src/index.php
+++ src/index.php
@@ -11,7 +11,8 @@ class Index
 
 	public function createNonceUrl($url, $postWithId, $nonceName = "my_nonce")
 	{	
-		$field = "<input type='hidden' name='".$nonceName."' value='".$this->createNonceField()."'>";
+		$action = "";
+		$field = "<input type='hidden' name='".$nonceName."' value='".wp_create_nonce( $action )."'>";
 		return $field;
 	}
 }
